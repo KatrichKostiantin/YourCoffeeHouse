4c018e811643af16319031f203bd45cc00801b79
Konstantin <59829955+KatrichKostiantin@users.noreply.github.com>
2020-12-13 22:04:53 +0200

    First init

---
+++ src/main/java/com/example/ych/YchApplication.java
@@ -0,0 +1,13 @@
+package com.example.ych;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ych/YchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ych;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
